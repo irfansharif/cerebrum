c2a8ae5393473e0db44e66d95e7993f1cf31b397
Arham Ahmed <a.arhamIT@gmail.com>
2016-04-09 18:31:02 -0400

    completed train function

--- lib/cerebrum/cerebrum.rb
+++ lib/cerebrum/cerebrum.rb
@@ -11,6 +11,14 @@ class Cerebrum
     @hidden_layers  = hidden_layers
   end
 
+  def initialize_n_n
+    # TODO
+  end
+
+  def train_pattern(x, y)
+    # TODO
+  end
+
   def train(training_set, options = Hash.new)
     training_set = scrub_data_set(training_set)
 
@@ -25,5 +33,33 @@ class Cerebrum
 
     hidden_layers = [ [3, (input_size/2).floor].max ] unless @hidden_layers
     sizes = [input_size, hidden_layers, output_size].flatten
+
+    initialize_n_n(sizes)
+
+    error = 1
+    iteration_num = 0
+    while iteration_num < iterations && error > error_thresh
+      sum = 0
+      set_iteration = 0
+      while set_iteration < training_set.length
+        err = train_pattern(
+                training_set[set_iteration][:input],
+                training_set[set_iteration][:output],
+                learning_rate
+              )
+        sum = sum + err
+        set_iteration = set_iteration + 1
+      end
+
+      error = sum / training_set.length
+
+      if (log && iteration_num % log_period == 0)
+        p "iterations: #{iteration_num}, training error: #{error}"
+      end
+
+      iteration_num = iteration_num + 1
+    end
+
+   Hash[:error, error, :iterations, iteration_num]
   end
 end
